77b19bc3bfac7b5ccd94180e25b744f425d17555
PN-G <139358746+PN-G@users.noreply.github.com>
2023-10-04 18:49:00 +0200

    ajout du carousel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,70 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// création des variables pour selectionner les élements du DOM
+
+let dotsContainer = document.querySelector(".dots")
+let arrowLeft = document.querySelector(".arrow_left")
+let arrowRight = document.querySelector(".arrow_right")
+let bannerImg = document.querySelector(".banner-img")
+let bannerTxt = document.querySelector("#banner p")
+let slidesNumber = slides.length
+let index = 0
+
+// ajout du nombre de points en fonction du nombre d'images dans le tableau
+
+for (i=0 ; i<slidesNumber ; i++) {
+	let dot = document.createElement("div")
+	dot.classList.add("dot")
+	dotsContainer.appendChild(dot)
+}
+
+// ajout des event listeners sur les fleches de navigation
+
+arrowLeft.addEventListener("click", function() {
+	if (index === 0) {
+		index = slidesNumber - 1
+	} else {
+		index--
+	}
+	showCurrentSlide(index)
+	dotUpdate(index)
+})
+
+arrowRight.addEventListener("click", function() {
+	if (index === slidesNumber - 1) {
+		index = 0
+	} else {
+		index++
+	}
+	showCurrentSlide(index)
+	dotUpdate(index)
+})
+
+// fonction qui va ajouter ou retirer la classe dot_selected
+
+function dotUpdate(index) {
+	let dotList = document.querySelectorAll(".dot")
+
+	for (let dotIndex = 0; dotIndex<dotList.length ; dotIndex++) {
+		let currentDot = dotList[dotIndex]
+
+		if (dotIndex === index) {
+			currentDot.classList.add("dot_selected")
+		} else {	
+			currentDot.classList.remove("dot_selected")
+		}
+	}	
+}
+
+dotUpdate(index)
+
+// fonction qui met à jour le contenu de la banniere (image et texte)
+
+function showCurrentSlide(index) {
+	bannerImg.src = `./assets/images/slideshow/${slides[index].image}`
+	bannerTxt.innerHTML = `${slides[index].tagLine}`
+}
+
+
